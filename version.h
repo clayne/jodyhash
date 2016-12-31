f41931aa0d1c772e350d3bbf3c9db6984a8baa95
Jody Bruchon <jody@jodybruchon.com>
2016-12-31 17:40:29 -0500

    Update copyright years

--- version.h
+++ version.h
@@ -1,6 +1,9 @@
 /* Set version for jodyhash utility */
+
 #ifndef JODYHASH_VERSION_H
 #define JODYHASH_VERSION_H
+
 #define VER "4.1"
 #define VERDATE "2016-08-20"
+
 #endif	/* JODYHASH_VERSION_H */
