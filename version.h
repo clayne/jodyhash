4fc319703510f40dccf0e42af3b6f19e89a38076
Jody Bruchon <jody@jodybruchon.com>
2017-07-20 17:11:38 -0400

    Release v4.1.2

--- version.h
+++ version.h
@@ -3,7 +3,7 @@
 #ifndef JODYHASH_VERSION_H
 #define JODYHASH_VERSION_H
 
-#define VER "4.1.1"
-#define VERDATE "2017-03-23"
+#define VER "4.1.2"
+#define VERDATE "2017-07-20"
 
 #endif	/* JODYHASH_VERSION_H */
