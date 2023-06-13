bfc992a8761b58cebb237d6fc2b8c0c01d08b2a9
Jody Bruchon <jody@jodybruchon.com>
2023-06-13 18:09:28 -0400

    v7.3 release

--- version.h
+++ version.h
@@ -3,7 +3,7 @@
 #ifndef JODYHASH_VERSION_H
 #define JODYHASH_VERSION_H
 
-#define VER "7.2"
-#define VERDATE "2023-05-06"
+#define VER "7.3"
+#define VERDATE "2023-06-13"
 
 #endif	/* JODYHASH_VERSION_H */
